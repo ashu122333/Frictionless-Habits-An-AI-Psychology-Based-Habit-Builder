372b9c8f4ce3f789d0bfce265985acab32aed770
yadavharsh2004 <harshy8013698@gmail.com>
2025-11-29 22:12:07 +0530

    Update App.jsx before pull

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,10 @@ import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./components/Auth/Register";
 import Login from "./components/Auth/Login";
+import Dashboard from "./components/pages/Dashboard";
 import axios from "axios";
-// import HabitList from "./components/pages/HabitList.jsx";
+import HabitList from "./components/pages/HabitList.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
   const token = localStorage.getItem("token");
@@ -17,6 +19,7 @@ function App() {
     <Router>
 
       <Routes>
+        <Route path="/" element={<Dashboard/>} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/myhabits" element={<HabitList />} />
